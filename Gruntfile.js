9f5e6ecc198ba50ce2fd394cf909eb1923bda9aa
Oleg Smith <olegsmith@gmail.com>
2013-10-24 04:12:11 +0400

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+module.exports = function(grunt) {
+    /*jshint evil:true */
+
+
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-bg-shell');
+    grunt.loadNpmTasks('grunt-curl');
+    grunt.loadNpmTasks('grunt-zip');
+    //https://github.com/vkadam/grunt-jsbeautifier
+    //grunt.loadNpmTasks('grunt-jsbeautifier');
+
+    grunt.registerTask('default', ['build']);
+    //grunt.registerTask('build', ['clean', 'jshint', 'html2js', 'concat', 'uglify', 'copy']);
+    grunt.registerTask('build', ['concat']);
+    grunt.registerTask('install-components', ['curl-dir', 'unzip', 'copy']);
+
+    var gruntConfig = {
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' + ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;\n' + ' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */\n',
+        componentsdir: 'tourapp/components',
+        wwwcomponentsdir: 'tourapp/www/components',
+
+
+        'curl-dir': {
+            downzip: {
+                src: ['http://code.angularjs.org/1.2.0-rc.3/angular-1.2.0-rc.3.zip',
+                    'http://leaflet-cdn.s3.amazonaws.com/build/leaflet-0.6.4.zip',
+                    'https://github.com/topcoat/topcoat/archive/v0.8.0.zip',
+                    'http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.0.0.zip'
+                ],
+                dest: '<%= componentsdir %>'
+            }
+        },
+
+        'unzip': {
+            angular: {
+                src: '<%= componentsdir %>/angular-1.2.0-rc.3.zip',
+                dest: '<%= componentsdir %>'
+            },
+            leaflet: {
+                src: '<%= componentsdir %>/leaflet-0.6.4.zip',
+                dest: '<%= componentsdir %>/leaflet-0.6.4'
+            },
+            topcoat: {
+                src: '<%= componentsdir %>/v0.8.0.zip',
+                dest: '<%= componentsdir %>'
+            },
+            fontawesome: {
+                src: '<%= componentsdir %>/font-awesome-4.0.0.zip',
+                dest: '<%= componentsdir %>'
+            }
+        },
+
+        copy: {
+            angular: {
+                cwd: '<%= componentsdir %>/angular-1.2.0-rc.3/',
+                src: ['angular.min.js',
+                    'angular-touch.min.js'
+                ],
+                dest: '<%= wwwcomponentsdir %>/angular/', //,
+                flatten: true,
+                expand: true,
+                filter: 'isFile'
+            },
+
+            leaflet: {
+                cwd: '<%= componentsdir %>/leaflet-0.6.4/',
+                src: ['**', '!leaflet-src.js', '!leaflet.ie.css'],
+                dest: '<%= wwwcomponentsdir %>/leaflet/',
+                expand: true
+            },
+
+            fontawesome: {
+                cwd: '<%= componentsdir %>/font-awesome-4.0.0/',
+                src: ['css/font-awesome.min.css', 'fonts/**'],
+                dest: '<%= wwwcomponentsdir %>/font-awesome/',
+                expand: true
+            },
+
+            topcoat: {
+                cwd: '<%= componentsdir %>/topcoat-0.8.0/',
+                src: ['css/topcoat-mobile-light.min.css',
+                    'font/SourceSansPro-Regular.otf',
+                    'font/SourceSansPro-Light.otf',
+                    'font/SourceSansPro-Semibold.otf',
+                    'img/**'
+                ],
+                dest: '<%= wwwcomponentsdir %>/topcoat/',
+                expand: true
+            }
+
+        },
+
+        // concat: {
+        //     options: {
+        //       separator: ';',
+        //     },
+        //     js: {
+        //       src: ['src/intro.js', 'src/project.js', 'src/outro.js'],
+        //       dest: 'dist/built.js',
+        //     },
+        //   },
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    keepalive: true
+                }
+            }
+        }
+    };
+
+    /*grunt.log.writeln(JSON.stringify(gruntConfig, null, 4));*/
+
+    grunt.initConfig(gruntConfig);
+};
---
+++ tourapp/www/js/index.js
@@ -0,0 +1,114 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        } else {
+            this.onDeviceReady();
+        }
+
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+
+    // var slideMenuButton = document.getElementById('slide-menu-button');
+    // slideMenuButton.onclick = function(e) {
+    //     console.log('qwe');
+    //     var site = document.getElementById('app');
+    //     var cl = site.classList;
+    //     if (cl.contains('open-left-menu')) {
+    //         cl.remove('open-left-menu');
+    //     } else {
+    //         cl.add('open-left-menu');
+    //     }
+    // };
+
+
+ var showCodeDivs = document.getElementsByClassName('showcode');
+for (var i = showCodeDivs.length - 1; i >= 0; i--) {
+showCodeDivs[i].firstChild.onclick = function(e) {
+var element = e.target.parentNode.nextSibling.nextSibling;
+var style = window.getComputedStyle(element);
+if(style.getPropertyValue('display') == 'none'){
+e.target.innerHTML = 'Hide code snippets';
+element.style.display = 'block';
+} else {
+e.target.innerHTML = 'Show code snippets';
+element.style.display = 'none';
+}
+return false;
+};
+};
+var slideMenuButton = document.getElementById('slide-menu-button');
+slideMenuButton.onclick = function(e) {
+var site = document.getElementById('site');
+var cl = site.classList;
+if (cl.contains('open')) {
+cl.remove('open');
+} else {
+cl.add('open');
+}
+};
+
+//console.log(angular.element(document.getElementById('map')));
+//console.log(window.innerHeight);
+//console.log(document.getElementById('map').offsetTop);
+
+var mapEL=document.getElementById('map');
+mapEL.style.height=(window.innerHeight-mapEL.offsetTop)+"px";
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data © OpenStreetMap contributors'
+        }).addTo(map);
+
+
+window.onresize=function(){
+var mapEL=document.getElementById('map');
+mapEL.style.height=(window.innerHeight-mapEL.offsetTop)+"px";
+map.invalidateSize(true);
+};
+
+
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
